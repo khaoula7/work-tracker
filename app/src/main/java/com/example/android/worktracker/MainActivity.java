34f90c54660a8a326e90dd96a25801b4f61b0c81
Khaoula <tej.khaoula@gmail.com>
2019-02-18 23:14:03 +0400

    built activity_main and dialog_editor layouts to add a new project

--- app/src/main/java/com/example/android/worktracker/MainActivity.java
+++ app/src/main/java/com/example/android/worktracker/MainActivity.java
@@ -1,13 +1,51 @@
 package com.example.android.worktracker;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Click on Floating Action button will open editor_dialog
+        fab = (FloatingActionButton) findViewById(R.id.floating_action_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEditorDialog();
+            }
+        });
+    }
+
+    /**
+     * Builds the editor_dialog to allow user to create a new project
+     */
+    private void showEditorDialog() {
+        //Create the AlertDialog builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        //Inflate and set the layout for the AlertDialog
+        builder.setView(getLayoutInflater().inflate(R.layout.dialog_editor, null))
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    //Save new Project
+                    }
+                 })
+                .setNegativeButton("Discard", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        //Close the Alert Dialog
+                        dialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
